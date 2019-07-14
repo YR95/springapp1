cc20c963b22f093542da208c5a9208e96fb8f069
JuliaPustovaya <pustovayajulia@gmail.com>
2019-07-14 21:00:21 +0300

    Add factory method

--- src/main/java/ru/romaniuk/ClassicMusic.java
+++ src/main/java/ru/romaniuk/ClassicMusic.java
@@ -6,12 +6,19 @@ public class ClassicMusic implements Music {
         return "Classic";
     }
 
+    private ClassicMusic() {
+    }
+
+    public static ClassicMusic getClassicalMusic() {
+        System.out.println("factory creating");
+        return new ClassicMusic();
+    }
+
     public void doInitMethod() {
         System.out.println("Init classic ");
     }
 
-    public void destroyMethod()
-    {
+    public void destroyMethod() {
         System.out.println("Destroy classic");
     }
 }
