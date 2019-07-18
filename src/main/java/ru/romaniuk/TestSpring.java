d2aeeeff76bd6345668b13e0a76f93a714fdd1b7
O_Yuliia Pustova <ypustova@luxoft.com>
2019-07-18 17:39:10 +0300

    Autowiring byName

--- src/main/java/ru/romaniuk/TestSpring.java
+++ src/main/java/ru/romaniuk/TestSpring.java
@@ -5,7 +5,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
 	public static void main(String[] args) throws PerformerEcxeption {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		context.getBean("poetStar", Performer.class).perform();
 		context.getBean("kenny", Performer.class).perform();
 		
 	}
